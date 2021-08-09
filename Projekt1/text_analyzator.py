66944084211c9571309ea204a85867f63c793060
katkafialova <78508154+katkafialova@users.noreply.github.com>
2021-08-09 22:48:04 +0200

    Update text_analyzator.py

--- Projekt1/text_analyzator.py
+++ Projekt1/text_analyzator.py
@@ -23,7 +23,7 @@ which are about 300 feet thick.''',
 '''The monument contains 8198 acres and protects 
 a portion of the largest deposit of freshwater fish 
 fossils in the world. The richest fossil fish deposits 
-are found in multiple . limestone layers, which lie some 
+are found in multiple limestone layers, which lie some 
 100 feet below the top of the butte. The fossils 
 represent several varieties of perch, as well as 
 other freshwater genera and herring similar to those 
